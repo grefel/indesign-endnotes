f06dde24490238bcc8ae7513deb10a030e38561a
Gregor Fellenz <gregor.fellenz@publishingx.de>
2017-02-13 11:27:29 +0100

    test update

--- test/test_endnotes.jsx
+++ test/test_endnotes.jsx
@@ -2,7 +2,7 @@
 #include "idsHelper.jsx"
 #include "../createEndnotes.jsx"
 #include "../jumpBetweenMarkerAndNote.jsx"
-#include "../deleteEndnote.jsx"
+#include "../deleteEndnote.jsx" 
 
 
 
@@ -26,14 +26,8 @@ function runTests() {
 
 	// Run Integration Tests 
 	app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
-//~ 	basicIntegrationTest();
 	
-	testManualNumbering();
-	
-//~ 	test_01();
-//~ 	test_02();
-//~ 	test_03();
-//~ 	test_04();
+	basicIntegrationTest();	
 
 	// Show Test results 
 	idsTesting.htmlReport();
@@ -64,50 +58,11 @@ function basicIntegrationTest() {
 
 	idsTesting.insertBlock("Endnotes created?");
 
-	idsTesting.assertEquals("14 Hyperlinks im Dokument", 14, dokTest.hyperlinks.length );
-	idsTesting.assertEquals("7 paragraphDestinations im Dokument", 7, dokTest.paragraphDestinations.length );
-	idsTesting.assertEquals("7 crossReferenceSources im Dokument", 7, dokTest.crossReferenceSources.length );
-	idsTesting.assertEquals("7 hyperlinkTextDestinations im Dokument", 7, dokTest.hyperlinkTextDestinations.length );
-	idsTesting.assertEquals("7 hyperlinkTextSources im Dokument", 7, dokTest.hyperlinkTextSources.length );
-
-	idsTesting.insertBlock("Special Test?");
-
-	
-	var resultString = idsTools.readTextFile(px.logFile);	
-//~ 	idsTesting.assertStringInFile("Correct Error Message if Endnotes do not reside in the same story", localize(px.ui.endnoteStoryMoved) , px.logFile);
-
-	if (closeTestDok) {
-		dokTest.close(SaveOptions.NO);
-	}
-}
-
-
-// Beim nachträglichen Einfügen von Fußnoten muss sich das Endnotenverzeichnis noch in der gleichen Story befinden. 
-function testManualNumbering() {
-	idsTesting.insertBlock("Create Endnotes from Footnotes");
-	idsTesting.insertBlock("Check Document");
-	var testFile = File(getScriptFolderPath() + "/publicTestFiles/endnoteTestManualNumbering.idml");
-	var dokTest = app.open(testFile);
-	
-	idsTesting.assertEquals("Keine Hyperlinks im Dokument", 0, dokTest.hyperlinks.length );
-	idsTesting.assertEquals("Keine paragraphDestinations im Dokument", 0, dokTest.paragraphDestinations.length );
-	idsTesting.assertEquals("Keine crossReferenceSources im Dokument", 0, dokTest.crossReferenceSources.length );
-	idsTesting.assertEquals("Keine hyperlinkTextDestinations im Dokument", 0, dokTest.hyperlinkTextDestinations.length );
-	idsTesting.assertEquals("Keine hyperlinkTextSources im Dokument", 0, dokTest.hyperlinkTextSources.length );
-	
-	var endnoteStory = getEndnoteStory(dokTest);
-	getStyleInformation (dokTest);
-
-	readStyles(dokTest);	
-	foot2end(dokTest, endnoteStory);
-
-	idsTesting.insertBlock("Endnotes created?");
-
-	idsTesting.assertEquals("14 Hyperlinks im Dokument", 14, dokTest.hyperlinks.length );
-	idsTesting.assertEquals("7 paragraphDestinations im Dokument", 7, dokTest.paragraphDestinations.length );
-	idsTesting.assertEquals("7 crossReferenceSources im Dokument", 7, dokTest.crossReferenceSources.length );
-	idsTesting.assertEquals("7 hyperlinkTextDestinations im Dokument", 7, dokTest.hyperlinkTextDestinations.length );
-	idsTesting.assertEquals("7 hyperlinkTextSources im Dokument", 7, dokTest.hyperlinkTextSources.length );
+	idsTesting.assertEquals("11 Hyperlinks im Dokument", 11, dokTest.hyperlinks.length );
+	idsTesting.assertEquals("11 paragraphDestinations im Dokument", 11, dokTest.paragraphDestinations.length );
+	idsTesting.assertEquals("11 crossReferenceSources im Dokument", 11, dokTest.crossReferenceSources.length );
+	idsTesting.assertEquals("0 hyperlinkTextDestinations im Dokument", 0, dokTest.hyperlinkTextDestinations.length );
+	idsTesting.assertEquals("0 hyperlinkTextSources im Dokument", 0, dokTest.hyperlinkTextSources.length );
 
 	idsTesting.insertBlock("Special Test?");
 
@@ -122,80 +77,6 @@ function testManualNumbering() {
 
 
 
-// Beim nachträglichen Einfügen von Fußnoten muss sich das Endnotenverzeichnis noch in der gleichen Story befinden. 
-function test_01() {
-	idsTesting.insertBlock("With subsequent insertion of footnotes the endnote directory must still be in the same story");
-	var testFile = File(getScriptFolderPath() + "/localTestFiles/Fussnotentest_nachtraeglicheEndnotenAmAnfang.indd");
-	var dokTest = app.open(testFile);
-
-	var endnoteStory = getEndnoteStory(dokTest);
-	getStyleInformation (dokTest);
-	readStyles(dokTest);
-	foot2end(dokTest, endnoteStory);
-
-	var resultString = idsTools.readTextFile(px.logFile);
-	
-	idsTesting.assertStringInFile("Correct Error Message if Endnotes do not reside in the same story", localize(px.ui.endnoteStoryMoved) , px.logFile);
-
-	dokTest.close(SaveOptions.NO);
-}
-
-
-// U_Backmatter_Num-Formate mit umsetzen ... 
-function test_02() {
-	idsTesting.insertBlock("Problems with U_Backmatter_Num-Formate");
-	var testFile = File(getScriptFolderPath() + "/localTestFiles/num-Formate.indd");
-	var dokTest = app.open(testFile);
-	
-	var endnoteStory = getEndnoteStory(dokTest);
-	getStyleInformation (dokTest);
-	readStyles(dokTest);
-	foot2end(dokTest, endnoteStory);
-	
-	var resultString = idsTools.readTextFile(px.logFile);
-// ?? 
-//	idsTesting.assertStringInFile("Correct Error Message if Endnotes do not reside in the same story", localize(px.ui.endnoteStoryMoved) , px.logFile);
-
-	dokTest.close(SaveOptions.NO);
-}
-
-// alte Endnoten werden entfernt, Vor Kapitel 4 wird Inhalt merkwürdig zerpflückt, die Überschrift der neu generierten Endnoten ist inkorrekt. 
-function test_03() {
-	idsTesting.insertBlock("Alte Endnoten werden entfernt, Vor Kapitel 4 wird Inhalt merkwürdig zerpflückt, die Überschrift der neu generierten Endnoten ist inkorrekt.");
-	var testFile = File(getScriptFolderPath() + "/localTestFiles/Fußnotentest_vor_neuen_Kap.indd");
-	var dokTest = app.open(testFile);
-	
-	var endnoteStory = getEndnoteStory(dokTest);
-	getStyleInformation (dokTest);
-	readStyles(dokTest);
-	foot2end(dokTest, endnoteStory);
-	
-	var resultString = idsTools.readTextFile(px.logFile);
-// ?? 
-//	idsTesting.assertStringInFile("Correct Error Message if Endnotes do not reside in the same story", localize(px.ui.endnoteStoryMoved) , px.logFile);
-
-	dokTest.close(SaveOptions.NO);
-}
-
-
-// 
-// Endnotenverweis in (Kapitel-)Überschriften 
-function test_04() {
-	idsTesting.insertBlock("Endnotenverweis in (Kapitel-)Überschriften werden nicht verarbeitet.");
-	var testFile = File(getScriptFolderPath() + "/localTestFiles/Fussnotentest_FN_in_Ueberschrift.indd");
-	var dokTest = app.open(testFile);
-	
-	var endnoteStory = getEndnoteStory(dokTest);
-	getStyleInformation (dokTest);
-	readStyles(dokTest);
-	foot2end(dokTest, endnoteStory);
-	
-	var resultString = idsTools.readTextFile(px.logFile);
-	
-	idsTesting.assertStringInFile("Correct Error Message if Endnotes do not reside in the same story", localize(px.ui.endnoteStoryMoved) , px.logFile);
-
-	dokTest.close(SaveOptions.NO);
-}
 
 
 /** Get Filepath from current script  */
